eaacfc7d46fde197b3fe7c28c1a9f5ac1165c147
bohenderson <bohende@gmail.com>
2017-09-07 14:27:03 -0700

    Add script for combining clipping files

---
+++ coalesce.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+input="My Clippings.txt"
+output="my-clippings.txt"
+
+# Sanity Checks
+if [[ ! -f "$input" ]]; then echo "Nothing to coalesce"; exit;
+fi
+
+# Move $output to a temporary read location so we can 
+#   write to this filename at the other end of our pipe
+if [[ -f "$output" ]]; then mv "$output" ".$output.backup"
+fi
+
+# Send our input and old output into the pipe
+cat "$input" ".$output.backup" |\
+
+# Remove byte order markers
+sed 's/^\xef\xbb\xbf//' |\
+
+# Transform everything into one giant line
+tr -d '\n\r' |\
+
+# Split every highlight/bookmark/note onto it's own line
+sed 's/==========/==========\n/g' |\
+
+# Sort numerically so books w same title will sort by page/location
+# Remove any duplicate lines (we're idempotent!)
+sort -n | uniq |\
+
+# Put the newlines back where they were originally and we're done
+sed 's/==========/\n==========/g' |\
+sed 's/ \([AP]M\)/ \1\n\n/'       |\
+sed 's/- Your/\n- Your/'           \
+> "$output"
+
+# Remove our input & old output if everything went well
+if [[ $? -eq 0 && -f "$output" ]]; then
+  rm ".$output.backup" "$input"
+fi
+
