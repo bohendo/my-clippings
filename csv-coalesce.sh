87d390d6d699c142d5fd724683d8338c46133d06
bohenderson <bohende@gmail.com>
2017-09-12 17:14:16 -0700

    Add csv-coalesce script

---
+++ csv-coalesce.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Coalesces my-clippings.csv into my-clippings.txt as if it were My\ Clippings.txt
+
+input="my-clippings.csv"
+output="My Clippings.txt"
+
+if [[ ! -f "$input" ]]; then
+  echo "Nothing to coalesce"
+  exit 1
+fi
+if [[ -f "$output" ]]; then
+  echo "coalesce My\ Clippings.txt into my-clippings.txt first"
+  exit 1
+fi
+
+dos2unix $input 2> /dev/null
+
+title=`cat "$input" | sed '2q;d' | sed 's/^"//' | sed 's/",,,$//'`
+author=`cat "$input" | sed '3q;d' | sed 's/^"//' | sed 's/",,,$//'`
+
+tail -n +9 "$input" > "$input.clean"
+
+while read line; do
+
+  loc=`echo $line | awk -F "\",\"" '{print $2}' | tr -d '"'`
+  hlt=`echo $line | awk -F "\",\"" '{print $4}' | tr -d '"'`
+
+  echo "=========="
+  echo "$title ($author)"
+  echo "- Your Highlight on $loc | Added someday at sometime PM"
+  echo ""
+  echo "$hlt"
+  
+done < "$input.clean" > "$output"
+
+if [[ -f $output ]]; then
+  rm "$input.clean"
+fi
+
