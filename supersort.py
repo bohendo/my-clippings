53aa2774747bc2e340ec89feb213c34bdf8fff6a
Bo Henderson <bohende@gmail.com>
2017-10-19 11:14:00 -0700

    Clean docker highlights & fix sort bug

--- supersort.py
+++ supersort.py
@@ -8,10 +8,10 @@ import sys
 data = []
 for line in fileinput.input():
   title = str(re.search('^(.+)- Your ', line).group(1))
-  loc = int(re.search('(?:[Ll]ocation|[Pp]age) ([0-9]+)-?[0-9]* | Added on', line).group(1))
+  loc = int(re.search('(?:[Ll]ocation|[Pp]age)\s([0-9]+)-?[0-9]*\s\|\sAdded\son\s', line).group(1))
   # print "TITLE:  " + title
-  # print "LOC:    " + loc
-  # print "LINE:   " + line
+  # print("LOC:    " + loc)
+  # print("LINE:   " + line)
   data.append((title, loc, line))
   
 for i in [x[2] for x in sorted(data, key = lambda y: (str(y[0]), int(y[1])))]:
